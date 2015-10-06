d32b009af507ced72f964361bdf5f992901a642e
Matheus Rosa <matheusdsrosa@gmail.com>
2015-10-05 23:54:16 -0300

    Improved fuzzyfinder to return a sorted result

--- fuzzyfinder.rb
+++ fuzzyfinder.rb
@@ -1,10 +1,17 @@
+require 'enumerator'
+
 def fuzzyfinder(user_input, collection)
   suggestions = []
+  sorted = []
   pattern = user_input.split('').join('.*')
-  collection.each do |item|
-    if /#{pattern}/.match(item)
-      suggestions.push(item)
+  collection.each_index do |i|
+    item = collection[i]
+    match_data = item.match(/#{pattern}/)
+    if match_data
+      position = Regexp.last_match.begin(0)
+      suggestions.push([position, item])
     end
   end
-  suggestions
+  suggestions.sort!.each { |e| sorted.push(e[1]) }
+  sorted
 end
